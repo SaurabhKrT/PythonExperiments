c563c471eb27098c4dcc0997ad3e46a5ba58db33
Saurabh Tiwari <tiwari.saurabh4@gmail.com>
2018-12-27 16:21:24 +0530

    Update readPDF.py

--- readPDF.py
+++ readPDF.py
@@ -7,7 +7,7 @@ import os
 
 try:
   for file in os.listdir('folder path'):# lists the files in a given folder
-    if(file.endswith('.pdf') and file[:1] in '0123456789'):# checks if the file has .pdf and start with numbers
+    if(file.endswith('.pdf') and file[:1] in '0123456789'):# checks if the file ends with .pdf and start with numbers
       if file!=[]:# skips all empty lists
         parts = file.split('-')# split the the name into a list if - encountered
         if len(parts)>1:#checks if there are at least two elements in the list
